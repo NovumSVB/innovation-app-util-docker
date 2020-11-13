614a4846224d396e2178686b954ef5fbbcece37e
Anton Boutkam <anton@nui-boutkam.nl>
2020-11-13 21:21:51 +0100

    Removing unwanted stuff

--- api/repository-archive/20201113185206api.repository.demo.novum.nu/modules/ControllerFactory.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace ApiNovumRepository;
-
-use Core\IControllerFactory;
-
-class ControllerFactory extends \_DefaultApi\ControllerFactory implements IControllerFactory{}
--- api/repository-archive/20201113185206api.repository.demo.novum.nu/public_html/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-$aParts = array_reverse(explode('.', $_SERVER['HTTP_HOST']));
-
-$sPath = dirname($_SERVER['SCRIPT_FILENAME'], 3);
-
-require_once $sPath . '/index.php';
